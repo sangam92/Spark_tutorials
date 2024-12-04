5f76df2be2d5f5a15b7a7c649acc11d4d3f0a5d0
sangam92 <choubey.sangam@gmail.com>
2024-12-04 11:46:03 +0530

    adding groupby folder

---
+++ groupBy/df_groupby.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import sum,col,avg,min,max
+from pyspark.sql.types import IntegerType
+
+
+spark=SparkSession.builder.appName("groupby").getOrCreate()
+df=spark.read.csv('../data/employee.csv',header=True)
+
+##grouping the data 
+df=df.withColumn("Count",col("Count").cast(IntegerType()))
+df.printSchema()
+df1=df.groupBy("Color").agg(sum(col("Count"))).select(col("sum(Count)"),col("Color"))
+df.show()
+
+
+##Multiple groupby conditions
+df2=df.groupBy("State","Color").agg(sum("Count")).select("State","sum(Count)")
+df2.show()
+
+
+##Multiple aggregates at a time 
+df3= df.groupBy("State").agg(sum("Count"),avg("Count"),min("Count"),max("Count"))
+df3.show()
